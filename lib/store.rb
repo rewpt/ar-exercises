921c98e9311d83668273a2e16398efb6329e8d36
rewpt <zpermack@gmail.com>
2022-01-02 23:47:09 +0000

    add a validator for mens apparel and women's apparel both being false

--- lib/store.rb
+++ lib/store.rb
@@ -1,5 +1,15 @@
+class MyValidator < ActiveModel::Validator
+  def validate(record)
+    if record.womens_apparel == false && record.mens_apparel == false
+      record.errors.add :apparel, "Stores must sell womens or mens apparel"
+    end
+  end
+end
+
+
 class Store < ActiveRecord::Base
   has_many :employees
   validates :name, length: { minimum: 3, too_short: "%{count} characters is the minimum allowed" }
-  validates :annual_revenue, numericality: { only_integer: true, greater_than: 0}
+  validates :annual_revenue, numericality: { only_integer: true, greater_than: 0, message: "Annual Revenue must be an integer greater than 0"}
+  validates_with MyValidator
 end
