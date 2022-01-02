4cce413cb9b401ed30f8b7a9a8363a0de6278102
rewpt <zpermack@gmail.com>
2022-01-02 22:29:08 +0000

    add all validations for store

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,5 @@
 class Store < ActiveRecord::Base
-
+  has_many :employees
+  validates :name, length: { minimum: 3, too_short: "%{count} characters is the minimum allowed" }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than: 0}
 end
